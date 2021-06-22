3fe0d65aa92d553242856962ab743d87e1dac425
Simen A. W. Olsen <so@bjerk.io>
2021-06-22 15:34:05 +0200

    feat: Add Postgres configuration (#9)

--- config/database.js
+++ config/database.js
@@ -1,11 +1,28 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: env('CONNECTION', 'default'),
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
         client: 'sqlite',
         filename: env('DATABASE_FILENAME', '.tmp/data.db')
+      },
+      options: {
+        useNullAsDefault: true
+      }
+    },
+    production: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('POSTGRES_HOST'),
+        port: env.int('POSTGRES_PORT', 5432),
+        database: env('POSTGRES_NAME'),
+        username: env('POSTGRES_USERNAME'),
+        password: env('POSTGRES_PASSWORD'),
+        ssl: {
+          rejectUnauthorized: env.bool('POSTGRES_SSL_SELF', false) // For self-signed certificates
+        }
       },
       options: {
         useNullAsDefault: true
