ddccdf7a66a0d5d7e5b827c69e612dc3fffa6907
Simen A. W. Olsen <so@bjerk.io>
2021-06-25 12:16:52 +0200

    feat: add connection debug message

--- config/database.js
+++ config/database.js
@@ -1,32 +1,37 @@
-module.exports = ({ env }) => ({
-  defaultConnection: env('CONNECTION', 'default'),
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db')
-      },
-      options: {
-        useNullAsDefault: true
-      }
-    },
-    production: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('POSTGRES_HOST'),
-        port: env.int('POSTGRES_PORT', 5432),
-        database: env('POSTGRES_NAME'),
-        username: env('POSTGRES_USERNAME'),
-        password: env('POSTGRES_PASSWORD'),
-        ssl: {
-          rejectUnauthorized: env.bool('POSTGRES_SSL_SELF', false) // For self-signed certificates
+module.exports = ({ env }) => {
+  const connection = env('CONNECTION', 'default');
+  console.log(`Current Connection: ${connection}`);
+
+  return {
+    defaultConnection: connection,
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'sqlite',
+          filename: env('DATABASE_FILENAME', '.tmp/data.db')
+        },
+        options: {
+          useNullAsDefault: true
         }
       },
-      options: {
-        useNullAsDefault: true
+      production: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: env('POSTGRES_HOST'),
+          port: env.int('POSTGRES_PORT', 5432),
+          database: env('POSTGRES_NAME'),
+          username: env('POSTGRES_USERNAME'),
+          password: env('POSTGRES_PASSWORD'),
+          ssl: {
+            rejectUnauthorized: env.bool('POSTGRES_SSL_SELF', false) // For self-signed certificates
+          }
+        },
+        options: {
+          useNullAsDefault: true
+        }
       }
     }
-  }
-});
+  };
+};
