5479866490543b814c1cdd417e2f9e4cf260d23e
Jeff Reiffers <jeffreiffers@gmail.com>
2021-12-20 12:37:26 +0100

    chore: set smtp port

--- setup-msmtp.sh
+++ setup-msmtp.sh
@@ -8,14 +8,14 @@ touch /var/log/msmtprc && chmod 666 /var/log/msmtprc
 cat > /etc/msmtprc << EOL
 # Set default values for all following accounts.
 defaults
-port 25
 tls on
 tls_starttls on
 tls_certcheck off
 
 account default
 auth off
-host $SMTP_SERVER:$SMTP_PORT
+host $SMTP_SERVER
+port $SMTP_PORT
 domain $SMTP_HOSTNAME
 from FDK Content Mangement Service<datalandsbyen@norge.no>
 add_missing_date_header on
