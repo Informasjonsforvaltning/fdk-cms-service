b17befc37330438fc8b05290ab04bef295f351e6
Jeff Reiffers <jeffreiffers@gmail.com>
2021-12-21 12:08:50 +0100

    chore: set sendmail provider options

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,10 @@
 module.exports = ({ env }) => ({
   email: {
     provider: 'sendmail',
+    providerOptions: {
+      smtpPort: 587,
+      smtpHost: 'localhost'
+    },
     settings: {
       defaultFrom: 'datalandsbyen@norge.no',
       defaultReplyTo: 'datalandsbyen@norge.no',
