220cf0a971405c48c13cdfc6aabbc9918164d508
Jeff Reiffers <jeffreiffers@gmail.com>
2023-02-21 11:08:43 +0100

    fix: cors settings

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,15 +2,17 @@ export default ({ env }) => ([
   'strapi::errors',
   'strapi::security',
   {
-    name: 'strapi::cors',
-    config: {
-      origin: env.array('CORS_ORIGIN')
-    }
-  },
-  {
     name: 'strapi::poweredBy',
     config: {
       poweredBy: 'Felles datakatalog <data.norge.no>'
+    }
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: env.array('CORS_ORIGIN')
     }
   },
   'strapi::logger',
