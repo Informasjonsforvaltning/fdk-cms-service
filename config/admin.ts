74c6710be9f613f95caf66ba3acd73f4569ff18d
Jeff Reiffers <jeffreiffers@gmail.com>
2025-10-10 11:10:19 +0200

    improve: bump strapi

--- config/admin.ts
+++ config/admin.ts
@@ -4,5 +4,21 @@ export default ({ env }) => ({
   },
   auth: {
     secret: env('ADMIN_JWT_SECRET')
+  },
+  session: {
+    enabled: true,
+    client: 'cookie',
+    key: 'strapi.sid',
+    prefix: 'strapi:sess:',
+    secret: env('ADMIN_JWT_SECRET'),
+    cookie: {
+      path: '/',
+      httpOnly: true,
+      secure: env('NODE_ENV') === 'production',
+      sameSite: 'lax',
+      maxAge: 86400000, // 24 hours
+      overwrite: true,
+      signed: true
+    }
   }
 });
