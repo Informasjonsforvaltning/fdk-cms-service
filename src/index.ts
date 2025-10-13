b31347ab01ecfccebd6dab890ca9eef083cc353d
Jeff Reiffers <jeffreiffers@gmail.com>
2025-10-13 12:01:15 +0200

    fix: secure cookie

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export default {
+  register({ strapi }) {
+    // Force the socket to be treated as encrypted for proxy setups
+    strapi.server.use(async (ctx, next) => {
+      if (ctx.req?.socket) {
+        (ctx.req.socket as any).encrypted = true;
+      }
+      await next();
+    });
+  },
+
+  bootstrap() {}
+};
